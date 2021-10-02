55634192ea9996ae520a0fc4ca87f68f4cc09b5b
Jacob Quinn <quinn.jacobd@gmail.com>
2021-10-01 23:27:26 -0600

    Ensure string(::InlineString) returns String
    Fixes the type instability mentioned in
    https://github.com/JuliaData/InlineStrings.jl/issues/2.

--- src/InlineStrings.jl
+++ src/InlineStrings.jl
@@ -295,7 +295,7 @@ end
 end
 
 const BaseStrs = Union{Char, String, SubString{String}}
-Base.string(a::InlineString) = a
+Base.string(a::InlineString) = String(a)
 Base.string(a::InlineString...) = _string(a...)
 Base.string(a::BaseStrs, b::InlineString) = _string(a, b)
 Base.string(a::BaseStrs, b::BaseStrs, c::InlineString) = _string(a, b, c)
